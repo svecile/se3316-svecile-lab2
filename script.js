31809ead978131319c930167a767558535e83210
Spencer <svecile@uwo.ca>
2019-10-12 13:02:19 -0400

    Added checkout functionality
    Including a checkout button and when clicked a dialog will appear asking if you want to continue if you do it will empty your cart if you done it will put the items back as available.

--- script.js
+++ script.js
@@ -1,63 +1,87 @@
-
-
-function subForm(){
+function subForm() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var birthYear = document.getElementById("birth-year").value;
     var age = "Child";
 
     birthYear = parseInt(birthYear);
-  
-    if( name==""|| email==""){
+
+    if (name == "admin" && birthYear == 1867) {
+
+    }
+    if (name == "" || email == "") {
         window.alert("Please enter a value for all feilds!");
         return;
-    } else if (birthYear < 1900 || birthYear > 2019 ){
+    } else if (birthYear < 1900 || birthYear > 2019) {
         window.alert("Birth year out of range, please enter a birth year between 1900 and 2019!")
         return;
-    } else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))){
+    } else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
         window.alert("You have entered an invalid email address!");
         return;
     }
 
-    if (2019-birthYear >= 18){
+    if (2019 - birthYear >= 18) {
         age = "Adult";
     }
     var inFeilds = document.getElementById("dataIn") //get input feilds
     inFeilds.style.display = "none" //hide input feilds
 
-    var h = document.createElement("p"); //make new paragraph node
-    var t = document.createTextNode(name+" ("+email+") "+age); //sanatize input by adding it to a text node
-    h.appendChild(t);
+    var node = document.createElement("p"); //make new paragraph node
+    var txtNode = document.createTextNode(name + " (" + email + ") " + age); //sanatize input by adding it to a text node
+    node.appendChild(txtNode);
     var element = document.getElementById("h1")
-    element.appendChild(h);
+    element.appendChild(node);
 }
 
-function updateBasket(item){
+function updateBasket(item) {
     var node = document.getElementById(item);//get the node of the item that was added to the cart
     var image = node.getElementsByTagName("img")[0]; //get the img element
-    image.parentNode.removeChild(image); //remove the image element
+    image.style.display = "none";//make the image not displayed
 
     var due = document.createElement("p"); //create a new pargraph node
     due.innerHTML = node.getAttribute("data-due"); //get the data attribute containing the due date and add the string to the p node
     node.appendChild(due);//append the p node to the parent item node
 
     node.getElementsByTagName("button")[0].innerHTML = "Remove"; //change button text to remove
-    node.getElementsByTagName("button")[0].setAttribute("onclick", "remove(\""+ item + "\")"); //change onclick attribute to remove(item)
+    node.getElementsByTagName("button")[0].setAttribute("onclick", "remove(\"" + item + "\")"); //change onclick attribute to remove(item) where item is the id of the list item
     document.getElementById("basket").appendChild(node); //apend the edited item node to the basket list
 }
 
-function remove(item){
+function remove(item) {
     var node = document.getElementById(item); //get the items node
-    var image = document.createElement("img"); // create an img element
-    image.setAttribute("src", item +".jpg"); //add the file sorce to the image for the picture
-    image.setAttribute("width", "200");//add width attribute
-    image.setAttribute("height", "250");//add height attribute
-   
-    node.insertBefore(image, node.firstChild);
+    var image = node.getElementsByTagName("img")[0]; //get the img element
+    image.style.display = "inline-block"; //make the image visible again
+
     var due = node.getElementsByTagName("p")[0];
-    node.removeChild(due);
+    node.removeChild(due);//remove the due date
 
     node.getElementsByTagName("button")[0].innerHTML = "Add"; //change button text back to add
-    node.getElementsByTagName("button")[0].setAttribute("onclick", "updateBasket(\""+ item + "\")"); //change onclick attribute back to updateBasket(item)
+    node.getElementsByTagName("button")[0].setAttribute("onclick", "updateBasket(\"" + item + "\")"); //change onclick attribute back to updateBasket(item)
     document.getElementById("available-items").appendChild(node); //put the removed item node back on the available-items list
+}
+
+function checkout() {
+    var basketItems = document.getElementById("basket").getElementsByTagName("li"); //get all the list items in the basket and store them as an html collection
+    var x = confirm("Would you like to checkout? You have " + basketItems.length + " items in your cart"); //display comfermation box with how many items in cart
+
+    if (x == true) { //if you click on then remove all items from the checkout basket
+
+        document.getElementById("basket").innerHTML = "";
+        return;
+
+    } else { //if you click cancel return items to available list
+
+        var availableList = document.getElementById("available-items"); //get the list of available items
+
+        while (basketItems.length != 0) {//while there are still items in the basket
+
+            basketItems[0].getElementsByTagName("img")[0].style.display = "inline-block"; //get the first list item and make the picture visible again
+
+            var due = basketItems[0].getElementsByTagName("p")[0]; //remove the due date
+            basketItems[0].removeChild(due);
+
+            availableList.appendChild(basketItems[0]);//add the list item back to the available list
+        }
+        return;
+    }
 }
